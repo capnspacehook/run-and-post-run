28842c8035f4c468080ad9ee7c14caec60ae5145
Andrew LeFevre <jalefevre@liberty.edu>
2023-08-19 18:21:37 -0400

    attempt to make multiline scripts work

--- src/common.js
+++ src/common.js
@@ -4,48 +4,26 @@ const process = require("process");
 
 // read action inputs
 const input = {
-  run: core.getMultilineInput('run'),
-  post: core.getMultilineInput('post', {required: true}),
+  run: core.getInput('run'),
+  post: core.getInput('post', {required: true}),
   workingDirectory: core.getInput('working-directory'),
   shell: core.getInput('shell'),
   postShell: core.getInput('post-shell'),
 };
 
 export async function run() {
-  return runCommands(joinMultilineCommands(input.run), input.shell)
+  return runCommand(input.run, input.shell)
 }
 
 export async function post() {
-  return runCommands(joinMultilineCommands(input.post), input.postShell ? input.postShell : input.shell)
+  return runCommand(input.post, input.postShell ? input.postShell : input.shell)
 }
 
 /**
- * @param {String[]} commands
- * @return {String[]}
- */
-function joinMultilineCommands(commands) {
-  const result = []
-  const re = /\\\s*$/
-  const buf = []
-
-  for (const cmd of commands) {
-    buf.push(cmd.replace(re, '')) // push command into buffer
-
-    if (!re.test(cmd)) { // if command not ends with \
-      result.push(buf.join(' ')) // join buffer and push into result
-
-      buf.length = 0 // clear buffer
-    }
-  }
-
-  return result
-}
-
-/**
- * @param {String[]} commands
+ * @param {String} command
  * @param {String[]} shell
  */
-async function runCommands(commands, shell) {
+async function runCommand(command, shell) {
   /** @type {import('@actions/exec/lib/interfaces').ExecOptions} */
   const options = {
     cwd: input.workingDirectory,
@@ -58,17 +36,15 @@ async function runCommands(commands, shell) {
   }
 
   return (async () => {
-    for (const command of commands) {
-      if (command !== "") {
-        core.info(`\x1b[1m$ ${command}\x1b[0m`)
+    if (command !== "") {
+      core.info(`\x1b[1m$ ${command}\x1b[0m`)
 
-        let exitCode = shell === ""
-          ? await exec.exec(command, [], options)
-          : await exec.exec(shell, ['-c', command], options)
+      let exitCode = shell === ""
+        ? await exec.exec(command, [], options)
+        : await exec.exec(shell, ['-c', command], options)
 
-        if (exitCode !== 0) {
-          core.setFailed(`Command failed with exit code ${exitCode}`)
-        }
+      if (exitCode !== 0) {
+        core.setFailed(`Command failed with exit code ${exitCode}`)
       }
     }
   })().catch(error => core.setFailed(error.message))
